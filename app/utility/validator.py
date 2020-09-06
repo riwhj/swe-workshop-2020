08dad20071214b021b99598476bdfeb3f11607f6
jirawan chuaykeaw <jirawan.333388@gmail.com>
2020-09-06 13:44:34 +0700

    first commit

--- app/utility/validator.py
+++ app/utility/validator.py
@@ -1,6 +1,19 @@
-def validate_name(name):
-    return True
+def validate_name(name): 
 
+    num=["0","1","2","3","4","5","6","7","8","9"]
+    special=["!","@","#","%","$"]
+    space =" "
+
+    if name == "":
+        return False
+
+    for i in name:
+        is_num= i in num
+        is_special= i in special  
+        is_space= i in space
+        if is_num or is_special or is_space:
+            return False
+    return True
 
 def validate_id(id):
     return True
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -4,8 +4,24 @@ from app.utility.validator import validate_name, validate_id, validate_phone_num
 
 class TestUtility(unittest.TestCase):
     def test_validate_name_with_valid_input(self):
-        self.assertEqual(True, validate_name("hanif"))
+        self.assertEqual(True, validate_name("ดีมดี่"))
 
+    def test_validate_name_with_invalid_input_int(self):
+        self.assertEqual(False, validate_name("1234"))
+
+    def test_validate_name_with_invalid_input_string_and_int(self):
+        self.assertEqual(False, validate_name("ดีมดี่1234"))
+
+    def test_validate_name_with_invalid_input_special(self):
+        self.assertEqual(False, validate_name("@!#$%"))
+
+    def test_validate_name_with_invalid_input_space(self):
+        self.assertEqual(False, validate_name("ก่อ กฤษฎิ์"))
+
+    def test_validate_name_with_invalid_input_no_value(self):
+        self.assertEqual(False, validate_name(""))
+
+    
 
 if __name__ == '__main__':
     unittest.main()  # pragma: no cover
